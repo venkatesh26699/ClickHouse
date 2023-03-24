afa6af34efdd260c786a8911a4d01873ae768bdf
Robert Schulze <robert@clickhouse.com>
2023-03-24 14:38:40 +0000

    Mark query cache as non-experimental

--- tests/queries/0_stateless/02494_query_cache_drop_cache.sql
+++ tests/queries/0_stateless/02494_query_cache_drop_cache.sql
@@ -1,8 +1,6 @@
 -- Tags: no-parallel
 -- Tag no-parallel: Messes with internal cache
 
-SET allow_experimental_query_cache = true;
-
 -- Cache query result in query cache
 SELECT 1 SETTINGS use_query_cache = true;
 SELECT count(*) FROM system.query_cache;

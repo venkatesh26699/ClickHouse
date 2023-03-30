9168a9c52664f19f25df9eec9a7ea3c315ee3147
venkatesh26699 <48954990+venkatesh26699@users.noreply.github.com>
2023-03-30 16:25:26 +0530

    Update stdcxx.h

--- contrib/arrow-cmake/cpp/src/thrift/stdcxx.h
+++ contrib/arrow-cmake/cpp/src/thrift/stdcxx.h
@@ -6,6 +6,6 @@ Fixes
 ../contrib/arrow-cmake/cpp/src/parquet/parquet_types.h:18:10: fatal error: thrift/stdcxx.h: No such file or directory
 #include <thrift/stdcxx.h>
 
-Delete me.
+Delete me as soon as possible.
 
 */
